2d23fa3017c695c0c7076d3c4338ff1f18a8c611
devrubayet <rubayethossain2003@gmail.com>
2025-11-06 15:48:35 +0600

    update home page

--- resources/views/frontend/components/iframe.blade.php
+++ resources/views/frontend/components/iframe.blade.php
@@ -1,7 +1,14 @@
 <section class="py-5 bg-light text-center">
-    <div class="container">
-        <div class="ratio ratio-21x9">
-  <iframe src="https://www.youtube.com/embed/nFuTKZU4UDE?si=YZE3NAW8aZZmq9mQ" title="YouTube video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</div>
+  <div class="container">
+    <div class="ratio ratio-21x9">
+      <iframe
+        src="https://www.youtube.com/embed/nFuTKZU4UDE?autoplay=1&loop=1&playlist=nFuTKZU4UDE&controls=0&mute=1&modestbranding=1&rel=0"
+        title="YouTube video"
+        frameborder="0"
+        allow="autoplay; encrypted-media; picture-in-picture"
+        allowfullscreen
+        >
+      </iframe>
     </div>
+  </div>
 </section>
