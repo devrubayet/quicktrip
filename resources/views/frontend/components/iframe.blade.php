25b10d3b60cb49cf728fd7f966c07dfccb6c2895
devrubayet <rubayethossain2003@gmail.com>
2025-11-06 15:36:55 +0600

    update home page

--- resources/views/frontend/components/iframe.blade.php
+++ resources/views/frontend/components/iframe.blade.php
@@ -1,6 +1,6 @@
 <section class="py-5 bg-light text-center">
     <div class="container">
-        <div class="ratio ratio-16x9">
+        <div class="ratio ratio-21x9">
   <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?autoplay=1" title="YouTube video" allow="autoplay" allowfullscreen></iframe>
 </div>
     </div>
