576903e2eb6587883e09a37e1086aa6335fc3f67
devrubayet <rubayethossain2003@gmail.com>
2025-11-06 15:43:43 +0600

    update home page

--- resources/views/frontend/components/iframe.blade.php
+++ resources/views/frontend/components/iframe.blade.php
@@ -1,7 +1,7 @@
 <section class="py-5 bg-light text-center">
     <div class="container">
         <div class="ratio ratio-21x9">
-  <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?autoplay=1" title="YouTube video" allow="autoplay" allowfullscreen></iframe>
+  <iframe src="https://www.youtube.com/embed/nFuTKZU4UDE?si=YZE3NAW8aZZmq9mQ" title="YouTube video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </div>
     </div>
 </section>
