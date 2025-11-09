6bfa4c8c74dc9032f6018d00959a35741623637a
devrubayet <rubayethossain2003@gmail.com>
2025-11-09 16:47:55 +0600

    update iframe video

--- resources/views/frontend/components/iframe.blade.php
+++ resources/views/frontend/components/iframe.blade.php
@@ -5,7 +5,7 @@
     class="ratio ratio-21x9 rounded-5 border-0"
     id="ytplayer"
     type="text/html"
-    src="https://www.youtube.com/embed/14l4XH92mm8?autoplay=1&controls=0"
+    src="https://www.youtube.com/embed/14l4XH92mm8?autoplay=1&controls=0&loop=1&playlist=14l4XH92mm8"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     referrerpolicy="strict-origin-when-cross-origin"
