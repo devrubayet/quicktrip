588aaedafe938d21fc4c5e1d551a2581247a7231
devrubayet <rubayethossain2003@gmail.com>
2025-11-09 14:18:25 +0600

    update iframe video

--- resources/views/frontend/components/iframe.blade.php
+++ resources/views/frontend/components/iframe.blade.php
@@ -1,14 +1,9 @@
 <section class="py-5 bg-light text-center">
     <div class="container">
-        <div class="ratio ratio-16x9" style="width:100%; max-width:1200px; height:400px; margin:auto;">
-  <iframe
-    src="https://www.youtube.com/embed/JZnW4UrXF0I?si=TCZmuNMpEZ80Jjzp&amp;controls=0"
-    title="YouTube video player"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    referrerpolicy="strict-origin-when-cross-origin"
-    allowfullscreen
-    style="border:0;">
-  </iframe>
+        <div class="ratio ratio-16x9" >
+  <iframe id="ytplayer" type="text/html" width="720" height="405"
+src="https://www.youtube.com/embed/M7lc1UVf-VE"
+frameborder="0" allowfullscreen>
 </div>
 
     </div>
