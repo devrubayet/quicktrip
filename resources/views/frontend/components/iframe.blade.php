5ab6c196bcb9ad531873de26ffbc3f42330b421a
devrubayet <rubayethossain2003@gmail.com>
2025-11-09 13:58:47 +0600

    update iframe video

--- resources/views/frontend/components/iframe.blade.php
+++ resources/views/frontend/components/iframe.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="ratio ratio-21x9">
       <iframe
-        src="https://www.youtube.com/embed/nFuTKZU4UDE?autoplay=1&loop=1&playlist=nFuTKZU4UDE&controls=0&mute=1&modestbranding=1&rel=0"
+        src="https://www.youtube.com/embed/JZnW4UrXF0I?si=TCZmuNMpEZ80Jjzp&amp;controls=0"
         title="YouTube video"
         frameborder="0"
         allow="autoplay; encrypted-media; picture-in-picture"
