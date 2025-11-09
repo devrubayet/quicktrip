4dbd9b6eca63c27194343da1c2faf6575279837c
devrubayet <rubayethossain2003@gmail.com>
2025-11-09 14:37:30 +0600

    update iframe video

--- resources/views/frontend/components/iframe.blade.php
+++ resources/views/frontend/components/iframe.blade.php
@@ -1,8 +1,8 @@
 <section class="py-5 bg-light text-center">
     <div class="container">
-        <div class="ratio ratio-16x9 rounded-2">
+        <div class="ratio ratio-21x9 rounded-2">
   <iframe
-    class="ratio ratio-16x9 rounded-5 border-0"
+    class="ratio ratio-21x9 rounded-5 border-0"
     id="ytplayer"
     type="text/html"
     src="https://www.youtube.com/embed/JZnW4UrXF0I?autoplay=1&controls=0"
