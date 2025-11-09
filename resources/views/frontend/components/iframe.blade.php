085d922b0f392a26aad10aa78d7e980dca094465
devrubayet <rubayethossain2003@gmail.com>
2025-11-09 14:12:56 +0600

    update iframe video

--- resources/views/frontend/components/iframe.blade.php
+++ resources/views/frontend/components/iframe.blade.php
@@ -1,11 +1,15 @@
 <section class="py-5 bg-light text-center">
     <div class="container">
-        <div class="col-md-12 ratio ratio-21x9">
-            <iframe width="560" height="315"
-                src="https://www.youtube.com/embed/JZnW4UrXF0I?si=TCZmuNMpEZ80Jjzp&amp;controls=0"
-                title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
+        <div class="ratio ratio-16x9" style="width:100%; max-width:1200px; height:400px; margin:auto;">
+  <iframe
+    src="https://www.youtube.com/embed/JZnW4UrXF0I?si=TCZmuNMpEZ80Jjzp&amp;controls=0"
+    title="YouTube video player"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen
+    style="border:0;">
+  </iframe>
+</div>
+
     </div>
 </section>
