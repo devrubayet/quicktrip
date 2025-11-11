5892bfe8c2f9c5b646bb74cbf6d0d53be5d765de
devrubayet <rubayethossain2003@gmail.com>
2025-11-11 19:42:27 +0600

    visa-find url

--- resources/views/frontend/base/base.blade.php
+++ resources/views/frontend/base/base.blade.php
@@ -221,6 +221,9 @@
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
         AOS.init();
+        
+    </script>
+    <script>
         const visaFindUrl = {{ route('visa-find') }};
     </script>
     <script src="{{ asset('frontend/js/main.js') }}"></script>
